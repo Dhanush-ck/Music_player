0410bcdde7e13c275818deb37513522a27906518
Dhanush CK <dhanushck01@gmail.com>
2024-12-03 23:02:32 +0530

    feat : Created a dataset

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,25 @@
-const heart = document.getElementById('heart');
+const data = [{
+                name : "Dandelions",
+                src : "./assets/song/Dandelions(PagalNew.Com.Se).mp3",
+                image : "./assets/img/a6c65ab3a34710849e126359a707b37d.jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (1).jpg"
+            },
+            {
+                name : "Losing Interest",
+                src : "./assets/song/Shiloh_Dynasty_Itssvd_-_Losing_Interest(impuremusic.com).mp3",
+                image : "./assets/img/wallpaperflare.com_wallpaper (5).jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (3).jpg"
+            },
+            {
+                name : "",
+                src : "",
+                image : "./assets/img/wallpaperflare.com_wallpaper (7).jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (2).jpg"
+            }
+]
 
-function heartChange(e) {
-    if(e.src === "./assets/img/heart.png"){
-        e.src = "./assets/img/heart_fill.png"
-        console.log(1);
-    }
-    else {
-        console.log(2);
-        e.src = "./assets/img/heart.png";
-    }
-}
+const container = document.querySelector('.container');
+const coverImage = document.getElementById('cover-image');
+
+container.style.backgroundImage = `url('${data[0].bgImage}')`;
+coverImage.src = `${data[0].image}`;
