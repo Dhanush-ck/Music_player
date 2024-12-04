ba10b467e608fa0144454e7529c50a0fa8b0b2e0
Dhanush CK <dhanushck01@gmail.com>
2024-12-04 23:55:44 +0530

    Revert "menf"
    This reverts commit d3181b81dc787ce39816be5abddc58c461847cf7.

--- public/js/script.js
+++ public/js/script.js
@@ -1,91 +1,25 @@
-// dataset 
 const data = [{
                 name : "Dandelions",
                 src : "./assets/song/Dandelions(PagalNew.Com.Se).mp3",
-                image : "./assets/img/photos/cover1.jpg",
-                bgImage : "./assets/img/photos/bg1.jpg"
+                image : "./assets/img/a6c65ab3a34710849e126359a707b37d.jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (1).jpg"
             },
             {
                 name : "Losing Interest",
                 src : "./assets/song/Shiloh_Dynasty_Itssvd_-_Losing_Interest(impuremusic.com).mp3",
-                image : "./assets/img/photos/cover2.jpg",
-                bgImage : "./assets/img/photos/bg2.jpg"
+                image : "./assets/img/wallpaperflare.com_wallpaper (5).jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (3).jpg"
             },
             {
                 name : "",
                 src : "",
-                image : "./assets/img/photos/cover3.jpg",
-                bgImage : "./assets/img/photos/bg3.jpg"
+                image : "./assets/img/wallpaperflare.com_wallpaper (7).jpg",
+                bgImage : "./assets/img/wallpaperflare.com_wallpaper (2).jpg"
             }
 ]
 
-// background div 
 const container = document.querySelector('.container');
-// cover picture 
 const coverImage = document.getElementById('cover-image');
 
-// variable to hold the state of playing or not 
-var isPlaying = true;
-// data index 
-var dataIndex = 0;
-// play pause button 
-const playPause = document.getElementById('play-pause')
-// previous button 
-const prev = document.getElementById('prev')
-// next button  
-const next = document.getElementById('next')
-
-// function to change the src 
-function setPlayButton() {
-    if(isPlaying) {
-        playPause.src = "./assets/img/icons/play-button.png"
-        currentSong.pause();
-    }
-    else {
-        playPause.src = "./assets/img/icons/pause-button.png"
-        currentSong.play();
-    }
-    isPlaying = !isPlaying
-}
-
-// calling function on playpause button 
-playPause.onclick = ()=> {
-    setPlayButton();
-}
-
-// creating a audio element to play the song 
-const currentSong = document.createElement('audio');
-
-// function to change the index forwards 
-function nextTrack() {
-    (dataIndex<data.length-1)? dataIndex++: dataIndex=0;
-}
-
-// function to change the index backwards 
-function prevTrack() {
-    (dataIndex>0)? dataIndex--: dataIndex = data.length-1;
-}
-
-// calling function on clicking next 
-next.onclick = ()=> {
-    nextTrack()
-    setTrack()
-}
-
-// calling function on clicking prev 
-prev.onclick = ()=> {
-    prevTrack()
-    setTrack()
-}
-
-// function for setting the current track 
-function setTrack() {
-    container.style.backgroundImage = `url('${data[dataIndex].bgImage}')`;
-    coverImage.src = `${data[dataIndex].image}`;
-    currentSong.src = data[dataIndex].src;
-    currentSong.play();
-    isPlaying = true
-}
-
-// calling the function to set track intially 
-setTrack()
+container.style.backgroundImage = `url('${data[0].bgImage}')`;
+coverImage.src = `${data[0].image}`;
